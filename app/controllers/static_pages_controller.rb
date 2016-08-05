773c940e81ec954e6fbe5b67169fbc4e8b31ab94
Jorge Madrid <jlmadrid360@gmail.com>
2016-08-05 17:45:45 -0500

    Add Rails ORM
      * Wire the whole app using the rails router
      * Add heroku config

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :find_picture, except: [:index]
+  before_action :find_picture, except: [:index, :new, :create]
 
 	def index
     @pictures = Picture.all
@@ -7,15 +7,31 @@ class StaticPagesController < ApplicationController
 
 	def show; end
 
-	def new; end
+	def new
+    @picture = Picture.new
+  end
 
-  def create; end
+  def create
+    picture = Picture.create(picture_params)
+    if picture
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
 
-  def delete; end
+  def delete
+    @picture.destroy
+    redirect_to root_path
+  end
 
   private
 
   def find_picture
     @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:name, :description, :image_url)
   end
 end
